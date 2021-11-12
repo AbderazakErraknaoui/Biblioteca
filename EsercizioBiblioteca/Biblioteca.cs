e6ad272d4c98220a29cd4191df0330ed49c76be4
AbderazakErraknaoui <abderazak.erraknaoui.stud@ispascalcomandini.it>
2021-11-12 08:56:07 +0100

    Finita Classe Biblioteca

--- EsercizioBiblioteca/Biblioteca.cs
+++ EsercizioBiblioteca/Biblioteca.cs
@@ -8,5 +8,90 @@ namespace EsercizioBiblioteca
 {
     class Biblioteca
     {
+        private string _nome;
+        private string _indirizzo;
+        private DateTime _oraApertura, _oraChiusura;
+        private List<Libro> _libri;
+
+        public Biblioteca(string nome, string indirizzo, DateTime oraApertura, DateTime oraChiusura)
+        {
+            Nome = nome;
+            Indirizzo = indirizzo;
+            OraApertura = oraApertura;
+            OraChiusura = oraChiusura;
+            _libri = new List<Libro>();
+        }
+
+        public string Nome
+        {
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                    throw new Exception("Nome non valido");
+                else
+                    _nome = value;
+            }
+            get
+            {
+                return _nome;
+            }
+        }
+
+        public string Indirizzo
+        {
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                    throw new Exception("Indirizzo non valido");
+                else
+                    _indirizzo = value;
+            }
+            get
+            {
+                return _indirizzo;
+            }
+        }
+
+        public DateTime OraApertura
+        {
+            set
+            {
+                _oraApertura = value;
+            }
+            get
+            {
+                return _oraApertura;
+            }
+        }
+
+        public DateTime OraChiusura
+        {
+            set
+            {
+                if (value < _oraApertura)
+                    throw new Exception("Ora Chiusura non valida");
+                else
+                    _oraChiusura = value;
+            }
+            get
+            {
+                return _oraChiusura;
+            }
+        }
+
+        public void AggiuntaLibro(Libro b)
+        {
+            _libri.Add(b);
+        }
+
+        public List<Libro> GetLista()
+        {
+            return _libri;
+        }
+
+        public override string ToString()
+        {
+            return Nome + " " + Indirizzo + " " + OraApertura + " " + OraChiusura;
+        }
     }
 }
